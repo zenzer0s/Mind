35eeaadb3981641f4817385f88e099ea2323bc97
zenzer0s <praveenzalaki200@gmail.com>
2025-03-13 23:13:38 +0530

    Download media files before adding to the processing queue

--- bot/messageHandler.js
+++ bot/messageHandler.js
@@ -1,8 +1,32 @@
 const mediaQueue = require("./jobs/queue");
+const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
+const path = require("path");
 
 async function handleMediaRequest(userId, fileUrl, fileName) {
-  await mediaQueue.add("process-media", { userId, fileUrl, fileName });
-  console.log(`Added job for ${fileName} (${fileUrl})`);
+  // Download the file from Telegram
+  const filePath = path.join(__dirname, "../downloads", fileName);
+  const writer = fs.createWriteStream(filePath);
+
+  const response = await axios({
+    url: fileUrl,
+    method: "GET",
+    responseType: "stream",
+  });
+
+  response.data.pipe(writer);
+
+  await new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
+
+  console.log(`✅ Downloaded file: ${filePath}`);
+
+  // Add file to the queue
+  await mediaQueue.add("process-media", { userId, filePath, fileName });
+  console.log(`📥 Added job for ${fileName}`);
 }
 
 module.exports = handleMediaRequest;
